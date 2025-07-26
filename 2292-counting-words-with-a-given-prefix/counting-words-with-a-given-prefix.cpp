d13630526542aa126bb6b4305d493805c41f3802
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-07-26 06:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (22.29%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt=0;
+        
+        for(string word:words)
+        {
+            if(word.substr(0,pref.size())==pref)
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
