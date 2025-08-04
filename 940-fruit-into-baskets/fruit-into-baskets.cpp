acc52ff4cdefb593187b2f119d75192e62102504
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-08-04 09:31:47 +0530

    Time: 55 ms (12.17%) | Memory: 74.8 MB (90.37%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int>  mpp;
-        int n=fruits.size();
-        int maxi=0;
-        int l=0;
+       unordered_map<int,int> mpp;
+       int l=0;
+       int maxi=0;
+       int n=fruits.size();
 
-        for(int r=0;r<n;r++)
-        {
+       for(int r=0;r<n;r++)
+       {
             mpp[fruits[r]]++;
 
             while(mpp.size()>2)
@@ -22,8 +22,8 @@ public:
             }
 
             maxi=max(maxi,r-l+1);
-        }
+       }
 
-        return maxi;
+       return maxi;
     }
 };
