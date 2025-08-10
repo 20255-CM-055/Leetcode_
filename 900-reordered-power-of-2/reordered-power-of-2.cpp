68a78f199f27ebab018884c30fad731b19f3ea3e
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-08-10 10:09:37 +0530

    Time: 908 ms (5.01%) | Memory: 9.1 MB (51.03%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s=to_string(n);
+
+        sort(s.begin(),s.end());
+
+        do
+        {
+            if(s[0]=='0')
+            {
+                continue;
+            }
+            
+            for(int i=0;i<31;i++)
+            {
+                int num=stoi(s);
+                if((1<<i)==num)
+                {
+                    return true;
+                }
+            }
+        }while(next_permutation(s.begin(),s.end()));
+
+        return false;
+    }
+};
