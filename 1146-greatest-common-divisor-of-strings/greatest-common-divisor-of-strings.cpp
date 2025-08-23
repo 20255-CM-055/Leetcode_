870b41bd0e71369c42bb31ad66a787608b384588
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-08-23 20:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (18.85%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -3,13 +3,13 @@ public:
     string gcdOfStrings(string str1, string str2) {
         string ans="";
 
-        if(str1+str2!=str2+str1)
+        int n1=str1.size();
+        int n2=str2.size();
+
+        if(str1+str2 != str2+str1)
         {
             return "";
         }
-        
-        int n1=str1.size();
-        int n2=str2.size();
 
         int len=gcd(n1,n2);
 
