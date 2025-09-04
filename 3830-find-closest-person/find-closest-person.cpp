9f7d4164439b1c5db4ab1fcf187c5988238dac0d
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-09-04 14:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (21.23%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int ans=0;
+
+        if(abs(z-x) < abs(z-y))
+        {
+            ans=1;
+        }
+        else if(abs(z-x)==abs(z-y))
+        {
+            ans=0;
+        }
+        else 
+        {
+            ans=2;
+        }
+
+        return ans;
+    }
+};
