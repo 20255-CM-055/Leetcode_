36790e7f0423bc21ccd1d805432f65bd7b7dcfae
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-09-26 15:59:28 +0530

    Time: 32 ms (53.57%) | Memory: 16.5 MB (95.74%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -6,12 +6,21 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] + nums[j] > nums[k]) {
-                        cnt++;
-                    }
+        for(int k=n-1;k>=2;k--)
+        {
+            int i=0;
+            int j=k-1;
+
+            while(i<j)
+            {
+                if(nums[i]+nums[j]>nums[k])
+                {
+                    cnt=cnt+(j-i);
+                    j--;
+                }       
+                else
+                {
+                    i++;
                 }
             }
         }
