e7e924fa4f06f616dd2d37a3c91329902fa5273a
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2026-05-21 07:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (79.58%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        vector<char> ans;
         int l=0;
         int r=s.size()-1;
 
-        for(int i=r;i>=0;i--){
-            ans.push_back(s[i]);
+        while(l<r){
+            swap(s[l],s[r]);
+            l++;
+            r--;
         }
-
-        s=ans;
+        
     }
 };
